80fa3e5ba945330cbf9fbf2c7c75763eb13319b2
MuriloBarranco <murilo_cesar7@hotmail.com>
2023-11-17 17:24:42 -0300

    feature: new structure folders

---
+++ src/components/Sidebar/NavSection.tsx
@@ -0,0 +1,20 @@
+import { Box, Stack, Text } from "@chakra-ui/react"
+import { ReactNode } from "react"
+
+interface NavScetionProps {
+  title: string
+  children: ReactNode
+}
+
+export default function NavSection({ title, children}: NavScetionProps) {
+  return (
+    <Box>
+      <Text fontWeight="bold" color="gray.400" fontSize="small">
+        {title}
+      </Text>
+      <Stack spacing="4" mt="8" align="stetch">
+        {children}
+      </Stack>
+    </Box>
+  )
+}
